fcd6de535dfa673393789176bff57992d010f9d6
Elijahs Dev <63884351+ItsYaBoiElijah11@users.noreply.github.com>
2021-06-24 17:41:14 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const oriapi = require("oribotsapi");
+const dbl = new oriapi("TOKEN-HERE", client);
+
+client.on("ready", async () => {
+  dbl.serverCount();
+  // console.log("Server count posted")
+  let hasVote = await dbl.hasVoted("BOT-ID-HERE");
+  if(hasVote === true) {
+    console.log("Voted");
+  } else {
+    console.log("Vote please.");
+  }
+  
+  
+  let search = await dbl.search("BOT-ID-HERE");
+  console.log(search);
+});```
